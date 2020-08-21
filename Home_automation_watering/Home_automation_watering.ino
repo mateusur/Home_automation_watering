a3d38097549f845a7661b146f0469cba00685dcb
Mateusz Urbaniak <241614@student.pwr.edu.pl>
2020-08-21 10:30:47 +0200

    Callback added

--- Home_automation_watering/Home_automation_watering.ino
+++ Home_automation_watering/Home_automation_watering.ino
@@ -7,3 +7,16 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+void callback(char* topic, byte* payload, unsigned int length){
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  if((char)payload[0] =='1'){ //open Solenoid
+    } 
+  
+}
