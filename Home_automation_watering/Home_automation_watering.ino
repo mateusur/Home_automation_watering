8de97873337721096ece4d4e475f8fd75f739b24
Mateusz Urbaniak <241614@student.pwr.edu.pl>
2020-08-21 12:34:31 +0200

    Setup and small changes added

--- Home_automation_watering/Home_automation_watering.ino
+++ Home_automation_watering/Home_automation_watering.ino
@@ -1,13 +1,52 @@
-const char* topic_solenoid = "" // Topic you want to subscribe to 
+#include <WiFi.h>
+#include <PubSubClient.h>
+#define MSG_BUFFER_SIZE 50
+
+const char* ssid = ""; //Your WiFi ssid
+const char* password = ""; //Your WiFi password
+const char* server_ip = ""; //Sever name or ip(format xxx.xxx.x.x)
+int server_port = 1883; //Server port, usually 1883 or 8883
+const char* topic_solenoid = ""; // Topic you want to subscribe to 
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+char msg[MSG_BUFFER_SIZE];
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(server_ip, server_port);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(!client.connected())
+    reconnect();
+  client.loop();
 
 }
+
+void setup_wifi(){
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to: ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  delay(500);
+  WiFi.begin(ssid,password);
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+    }
+  randomSeed(micros());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
 void callback(char* topic, byte* payload, unsigned int length){
   Serial.print("Message arrived [");
   Serial.print(topic);
@@ -18,17 +57,19 @@ void callback(char* topic, byte* payload, unsigned int length){
   Serial.println();
 
   if((char)payload[0] =='1'){ //open Solenoid
-    } 
-  
+    }
+  else if((char)payload[0] == '0'){ //close Solenoid
+    }  
+}
 void reconnect(){
+  char* clientID = "ESP32_watering";
   while(!client.connected()){
-    char* clientID = "ESP32_watering";
-      if(client.connect(clientID){
+      if(client.connect(clientID)){
         client.subscribe(topic_solenoid);
         }
       else {
         Serial.print("failed, rc=");
-        Serial.print(client.state();
+        Serial.print(client.state());
         delay(5000);
         }
     }
